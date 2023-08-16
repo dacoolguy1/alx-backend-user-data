2ec850ef8c8eb47c769a8d1796a549a43b42a2bf
Emmanuel Temiede <68757523+dacoolguy1@users.noreply.github.com>
2023-08-16 17:02:58 +0100

    Add files via upload

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""flask authentication system"""
+"""flask authentication"""
 from flask import request
 from typing import List, TypeVar
 
@@ -17,7 +17,22 @@ class Auth():
         Returns:
             bool: _description_
         """
-        return False
+        if (path is None):
+            return True
+        if (excluded_paths is None or len(excluded_paths) == 0):
+            return True
+        path_slash = path.endswith("/")
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith("/"):
+                excluded_path = excluded_path[:-1]
+
+            if path_slash:
+                if path == excluded_path:
+                    return False
+            else:
+                if path == excluded_path or path + "/" == excluded_path:
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """_summary_
